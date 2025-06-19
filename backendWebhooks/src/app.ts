fa5448efe14ecebdedd7b98cf8abc01125c64fb9
felipertec <felipe.rtec@gmail.com>
2025-06-19 18:03:30 -0300

    Criado o caminho para o webhook realizado teste com o thunder client api e esta recebendo via Post

--- backendWebhooks/src/app.ts
+++ backendWebhooks/src/app.ts
@@ -6,6 +6,11 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
+app.post('/webhook', (req,res) => {
+    console.log(`Webhook Recebido:`, req.body);
+    res.status(200).send(`Webhook recebido com Sucesso!!!`)
+})
+
 app.listen(process.env.PORT,() => {
     console.log(`servidor iniciado em http://localhost:${process.env.PORT}`);
 })
