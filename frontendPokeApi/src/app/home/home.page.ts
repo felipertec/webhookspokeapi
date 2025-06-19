fca76c25b1e6d2c8f61a8eff76906e7a9e8a8df9
felipertec <felipe.rtec@gmail.com>
2025-06-19 20:42:22 -0300

    Realizado a busca de pokemons pela api pokeapi

--- frontendPokeApi/src/app/home/home.page.ts
+++ frontendPokeApi/src/app/home/home.page.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonGrid, IonRow } from '@ionic/angular/standalone';
+import { HttpService } from '../services/http.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, CommonModule, IonItem, IonGrid, IonRow],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+
+  public pokemons: any[] = [];
+
+  constructor(
+    private httpService: HttpService
+  ) {}
+
+  ngOnInit(): void {
+    this.getPokemons()
+  }
+
+  getPokemons(){
+    this.httpService.getPokemons().subscribe((data: any) => this.pokemons = data.results)
+  }
 }
