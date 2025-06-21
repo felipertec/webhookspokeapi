22261cb58350c232098bcf64d6ba790862027020
felipertec <felipe.rtec@gmail.com>
2025-06-21 11:17:59 -0300

    Criado lista de pokemons com imagens e nome dos pokemons

--- frontendPokeApi/src/app/home/home.page.ts
+++ frontendPokeApi/src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonGrid, IonRow } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonGrid, IonRow, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonLabel, IonThumbnail, IonList, IonCardContent } from '@ionic/angular/standalone';
 import { HttpService } from '../services/http.service';
 import { CommonModule } from '@angular/common';
 
@@ -7,21 +7,24 @@ import { CommonModule } from '@angular/common';
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, CommonModule, IonItem, IonGrid, IonRow],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, CommonModule, IonGrid,IonCard,IonCardHeader,IonCardTitle,IonCardSubtitle,IonItem,IonLabel,IonThumbnail,IonList,IonCardContent],
 })
 export class HomePage implements OnInit {
 
   public pokemons: any[] = [];
-
+  offset= 0;
   constructor(
     private httpService: HttpService
   ) {}
 
   ngOnInit(): void {
-    this.getPokemons()
+    this.loadPokemons()
   }
 
-  getPokemons(){
-    this.httpService.getPokemons().subscribe((data: any) => this.pokemons = data.results)
+  loadPokemons(){
+    this.httpService.getPokemons(this.offset).subscribe(data => {
+        console.log('resultado', data)
+        return this.pokemons = data;
+    })
   }
 }
--- frontendPokeApi/src/app/services/http.service.ts
+++ frontendPokeApi/src/app/services/http.service.ts
@@ -1,16 +1,33 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
+  baseUrl = 'https://pokeapi.co/api/v2'
+  imageUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
   constructor(
     private http: HttpClient
   ) { }
 
-  getPokemons(){
-    return this.http.get('https://pokeapi.co/api/v2/pokemon?limit=150')
+  getPokemons(offset: number = 0){
+    return this.http.get(`${this.baseUrl}/pokemon?offset=${offset}&limit=25`).pipe(map(result => {
+      return (result as any)['results'];
+    }),
+      map(pokemons => {
+        return pokemons.map((poke: any, index: any) => {
+          poke.image = this.getImagePokemon(index + offset + 1);
+          poke.pokeIndex = offset + index + 1;
+          return poke;
+        })
+      })
+    )
+  }
+
+  getImagePokemon(index: number){
+    return `${this.imageUrl}${index}.png`
   }
 }
